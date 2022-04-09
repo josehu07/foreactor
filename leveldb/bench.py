a0c8df03c65c8eb2109cdd190e1634acd3e20bd3
Guanzhou Hu <huguanzhou123@sina.com>
2022-04-09 00:54:56 -0500

    add leveldb get larger benchmark results

--- leveldb/bench.py
+++ leveldb/bench.py
@@ -6,10 +6,30 @@ import argparse
 
 
 URING_QUEUE_LEN = 256
+CGROUP_NAME = "leveldb_group"
 
 
-def run_ycsbcli_single(libforeactor, dbdir, trace, drop_caches, use_foreactor,
-                       pre_issue_depth=0):
+def set_cgroup_mem_limit(mem_limit):
+    cmd = ["sudo", "lscgroup"]
+    result = subprocess.run(cmd, check=True, capture_output=True)
+    output = result.stdout.decode('ascii')
+    cgroup_found = False
+    for line in output.split('\n'):
+        line = line.strip()
+        if line.startswith("memory:") and CGROUP_NAME in line:
+            cgroup_found = True
+            break
+
+    if not cgroup_found:    # create cgroup
+        cmd = ["sudo", "cgcreate", "-g", "memory:"+CGROUP_NAME]
+        subprocess.run(cmd, check=True)
+
+    cmd = ["sudo", "cgset", "-r", "memory.limit_in_bytes="+str(mem_limit),
+           CGROUP_NAME]
+    subprocess.run(cmd, check=True)
+
+def run_ycsbcli_single(libforeactor, dbdir, trace, mem_limit, drop_caches,
+                       use_foreactor, pre_issue_depth=0):
     os.system("sudo sync; sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'")
 
     envs = os.environ.copy()
@@ -22,6 +42,9 @@ def run_ycsbcli_single(libforeactor, dbdir, trace, drop_caches, use_foreactor,
            "--bg_compact_off", "--no_fill_cache"]
     if drop_caches:
         cmd.append("--drop_caches")
+    if mem_limit != "none":
+        set_cgroup_mem_limit(int(mem_limit))
+        cmd = ["sudo", "cgexec", "-g", "memory:"+CGROUP_NAME] + cmd
 
     result = subprocess.run(cmd, check=True, capture_output=True, env=envs)
     output = result.stdout.decode('ascii')
@@ -32,15 +55,16 @@ def run_ycsbcli_single(libforeactor, dbdir, trace, drop_caches, use_foreactor,
 def output_filename(output_prefix, output_suffix):
     return f'{output_prefix}-{output_suffix}.log'
 
-def run_exprs(libforeactor, dbdir, trace, drop_caches, output_prefix,
-              pre_issue_depth_list):
-    output = run_ycsbcli_single(libforeactor, dbdir, trace, drop_caches, False)
+def run_exprs(libforeactor, dbdir, trace, mem_limit, drop_caches,
+              output_prefix, pre_issue_depth_list):
+    output = run_ycsbcli_single(libforeactor, dbdir, trace, mem_limit,
+                                drop_caches, False)
     with open(output_filename(output_prefix, 'orig'), 'w') as fout:
         fout.write(output)
 
     for pre_issue_depth in pre_issue_depth_list:
-        output = run_ycsbcli_single(libforeactor, dbdir, trace, drop_caches,
-                                    True, pre_issue_depth)
+        output = run_ycsbcli_single(libforeactor, dbdir, trace, mem_limit,
+                                    drop_caches, True, pre_issue_depth)
         with open(output_filename(output_prefix, pre_issue_depth), 'w') as fout:
             fout.write(output)
 
@@ -55,14 +79,23 @@ def main():
                         help="trace file to run ycsbcli")
     parser.add_argument('-o', dest='output_prefix', required=True,
                         help="prefix of output log filenames")
+    parser.add_argument('--mem_limit', dest='mem_limit', required=False, default="none",
+                        help="memory limit to bound page cache size")
     parser.add_argument('--drop_caches', dest='drop_caches', action='store_true',
                         help="do drop_caches per request")
     parser.add_argument('pre_issue_depths', metavar='D', type=int, nargs='+',
                         help="pre_issue_depth to try")
     args = parser.parse_args()
 
-    run_exprs(args.libforeactor, args.dbdir, args.trace, args.drop_caches,
-              args.output_prefix, args.pre_issue_depths)
+    if args.mem_limit != "none":
+        try:
+            int(args.mem_limit)
+        except:
+            print("Error: mem_limit must be an integer in bytes")
+            exit(1)
+
+    run_exprs(args.libforeactor, args.dbdir, args.trace, args.mem_limit,
+              args.drop_caches, args.output_prefix, args.pre_issue_depths)
 
 if __name__ == "__main__":
     main()
--- leveldb/bplot.py
+++ leveldb/bplot.py
@@ -8,44 +8,49 @@ import matplotlib.pyplot as plt
 
 
 def read_avg_time_for_depth(input_logs):
-    assert len(input_logs) == 1
-    original_us = 0.0
-    pre_issue_depth_list, foreactor_us_list = [], []
+    assert len(input_logs) > 0
+    input_prefix = input_logs[0][:input_logs[0].rindex('-')]
+    for input_log in input_logs[1:]:
+        assert input_log.startswith(input_prefix)
 
-    with open(input_logs[0], 'r') as flog:
-        for line in flog.readlines():
-            line = line.strip()
-            if len(line) == 0:
-                continue
+    depth_avg_time = dict()
 
-            segs = line.split()
-            assert len(segs) == 2
-            if segs[0] == 'orig':
-                original_us = float(segs[1])
-            else:
-                pre_issue_depth_list.append(int(segs[0]))
-                foreactor_us_list.append(float(segs[1]))
+    for input_log in input_logs:
+        pre_issue_depth = input_log[input_log.rindex('-')+1:input_log.rindex('.')]
+        in_timing_section = False
 
-    return pre_issue_depth_list, original_us, foreactor_us_list
+        with open(input_log, 'r') as flog:
+            for line in flog.readlines():
+                line = line.strip()
+                if line.startswith("Removing top/bottom-5:"):
+                    in_timing_section = True
+                elif in_timing_section and line.startswith("avg "):
+                    time = float(line.split()[1])
+                    depth_avg_time[pre_issue_depth] = time
+                    break
 
-def plot_avg_time_for_depth(pre_issue_depth_list, original_us,
-                            foreactor_us_list, output_name):
+    return depth_avg_time
+
+def plot_avg_time_for_depth(depth_avg_time, output_name):
     width = 0.5
-    xs = list(range(1, len(pre_issue_depth_list) + 1))
+    xs = range(len(depth_avg_time))
 
-    plt.bar([0], [original_us], width,
+    depths = [int(d) for d in depth_avg_time.keys() if d != 'orig']
+    depths.sort()
+    depths = ['orig'] + [str(d) for d in depths]
+    ys = [depth_avg_time[d] for d in depths]
+
+    plt.bar(xs[:1], ys[:1], width,
             zorder=3, label="Original")
-    plt.bar(xs, foreactor_us_list, width,
+    plt.bar(xs[1:], ys[1:], width,
             zorder=3, label="Foreactor")
 
-    plt.text(0, original_us, f'{original_us:.3f}',
-             va='bottom', ha='center')
-    for x, y in zip(xs, foreactor_us_list):
+    for x, y in zip(xs, ys):
         plt.text(x, y, f'{y:.3f}',
                  va='bottom', ha='center')
 
     plt.ylabel("Time per Get (us)")
-    plt.xticks([0] + xs, ['original'] + pre_issue_depth_list)
+    plt.xticks(xs, depths)
     plt.xlabel("pre_issue_depth")
 
     plt.grid(zorder=0, axis='y')
@@ -56,10 +61,13 @@ def plot_avg_time_for_depth(pre_issue_depth_list, original_us,
     plt.close()
 
 def do_depth(input_logs, output_name):
-    pre_issue_depth_list, original_us, foreactor_us_list = \
-        read_avg_time_for_depth(input_logs)
-    plot_avg_time_for_depth(pre_issue_depth_list, original_us,
-                            foreactor_us_list, output_name)
+    depth_avg_time = read_avg_time_for_depth(input_logs)
+    plot_avg_time_for_depth(depth_avg_time, output_name)
+
+
+def do_mem(input_logs, output_name):
+    # TODO: implement this
+    return
 
 
 def read_all_times_for_cdf(input_logs):
@@ -83,23 +91,22 @@ def read_all_times_for_cdf(input_logs):
                     times = [float(t) for t in line.split()]
                     times.sort()
                     depth_times[pre_issue_depth] = times
-                    in_times_line = False
+                    break
 
-    print("Note: the largest value in each experiment is omitted to remove the\n"
-          "      setup overhead on the first request")
+    print("Note: the largest value in each experiment is omitted to remove the"
+          " setup overhead on the first request\n")
     for l in depth_times.values():
         del l[-1]
     return depth_times
 
 def plot_all_times_as_cdf(depth_times, output_name):
     max_time = max((l[-1] for l in depth_times.values()))
-    xs = [float(t) for t in range(0, int(max_time) + 10, 10)]
 
     depths = [int(d) for d in depth_times.keys() if d != 'orig']
     depths.sort()
-    depths = [str(d) for d in depths]
-    depths = ['orig'] + depths
+    depths = ['orig'] + [str(d) for d in depths]
 
+    print(f"{'depth':>5s} {'95':>8s} {'99':>8s} {'99.999':>8s}")
     for depth in depths:
         times = depth_times[depth]
         idx, xs, ys = 0, [], []
@@ -115,14 +122,22 @@ def plot_all_times_as_cdf(depth_times, output_name):
             if idx >= len(times):
                 break
 
-        # convert to percentage
-        ys = [cum_cnt / ys[-1] for cum_cnt in ys]
+        ys = [cum_cnt / ys[-1] for cum_cnt in ys]   # convert to percentage
+        tail_95 = times[int(len(times) * 0.95) - 1]
+        tail_99 = times[int(len(times) * 0.99) - 1]
+        tail_99_999 = times[int(len(times) * 0.99999) - 1]
+        print(f"{depth:>5s} {tail_95:>8.3f} {tail_99:>8.3f} {tail_99_999:>8.3f}")
 
-        plt.plot(xs, ys,
-                 zorder=3, label=depth)
+        p = plt.plot(xs, ys,
+                     zorder=3, label=depth)
+
+        plt.vlines(x=xs[-1], ymin=0.97, ymax=1.03,
+                   color=p[0].get_color())
 
     plt.ylabel("CDF Percentage (%)")
     plt.xlabel("Time per Get (us)")
+
+    plt.grid(zorder=0, axis='x')
 
     plt.legend()
 
@@ -134,22 +149,71 @@ def do_cdf(input_logs, output_name):
     plot_all_times_as_cdf(depth_times, output_name)
 
 
-def do_mem(input_logs, output_name):
-    return
-
-
-def do_skew(input_logs, output_name):
-    return
-
-
 def do_tail(input_logs, output_name):
+    # TODO: implement this
     return
+
+
+def read_times_on_samekey(input_logs):
+    assert len(input_logs) > 0
+    for input_log in input_logs:
+        assert "samekey-read" in input_log
+
+    depth_nread_times = dict()
+
+    for input_log in input_logs:
+        rdash = input_log.rindex('-')
+        pre_issue_depth = input_log[rdash+1:input_log.rindex('.')]
+        num_preads = int(input_log[input_log.rindex('-', 0, rdash)+1:rdash])
+        in_timing_section = False
+
+        with open(input_log, 'r') as flog:
+            for line in flog.readlines():
+                line = line.strip()
+                if line.startswith("Removing top/bottom-5:"):
+                    in_timing_section = True
+                elif in_timing_section and line.startswith("avg "):
+                    time = float(line.split()[1])
+                    if pre_issue_depth not in depth_nread_times:
+                        depth_nread_times[pre_issue_depth] = []
+                    while num_preads > len(depth_nread_times[pre_issue_depth]):
+                        depth_nread_times[pre_issue_depth].append(0)
+                    depth_nread_times[pre_issue_depth][num_preads - 1] = time
+                    break
+
+    return depth_nread_times
+
+def plot_times_on_samekey(depth_nread_times, output_name):
+    depths = [int(d) for d in depth_nread_times.keys() if d != 'orig']
+    depths.sort()
+    depths = ['orig'] + [str(d) for d in depths]
+
+    xs = range(1, len(depth_nread_times['orig']) + 1)
+
+    for depth in depths:
+        plt.plot(xs, depth_nread_times[depth],
+                 zorder=3, label=depth, marker='o')
+
+    plt.ylabel("Time of Get (us)")
+    plt.xticks(xs, xs)
+    plt.xlabel("Number of preads")
+
+    plt.grid(zorder=0, axis='y')
+    
+    plt.legend()
+
+    plt.savefig(output_name, dpi=120)
+    plt.close()
+
+def do_samekey(input_logs, output_name):
+    depth_nread_times = read_times_on_samekey(input_logs)
+    plot_times_on_samekey(depth_nread_times, output_name)
 
 
 def main():
     parser = argparse.ArgumentParser(description="Benchmark result plotting")
     parser.add_argument('-m', dest='mode', required=True,
-                        help="which figure to plot: depth|cdf|mem|skew|tail")
+                        help="which figure to plot: depth|mem|cdf|tail|samekey")
     parser.add_argument('-o', dest='output_name', required=True,
                         help="output plot filename")
     parser.add_argument('input_logs', metavar='I', type=str, nargs='+',
@@ -158,14 +222,14 @@ def main():
 
     if args.mode == 'depth':
         do_depth(args.input_logs, args.output_name)
-    elif args.mode == 'cdf':
-        do_cdf(args.input_logs, args.output_name)
     elif args.mode == 'mem':
         do_mem(args.input_logs, args.output_name)
-    elif args.mode == 'skew':
-        do_skew(args.input_logs, args.output_name)
+    elif args.mode == 'cdf':
+        do_cdf(args.input_logs, args.output_name)
     elif args.mode == 'tail':
         do_tail(args.input_logs, args.output_name)
+    elif args.mode == 'samekey':
+        do_samekey(args.input_logs, args.output_name)
     else:
         print(f'Error: mode {args.mode} unrecognized')
         exit(1)
