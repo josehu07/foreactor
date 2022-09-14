25e733ceef0f75b30d88794e061d8c7c4a71e7b3
josehu <josehu@localhost>
2022-09-13 22:08:14 -0600

    adapt eval plots to paper format

--- realapps/leveldb/scripts/plotter.py
+++ realapps/leveldb/scripts/plotter.py
@@ -134,8 +134,8 @@ def read_timer_fractions(results_dir, value_size, ycsb_distribution, backend,
 
 def plot_grouped_bars(results, x_list, x_label, y_label, output_prefix,
                       title_suffix):
-    plt.rcParams.update({'font.size': 16})
-    plt.rcParams.update({'figure.figsize': (14, 7)})
+    plt.rcParams.update({'font.size': 22})
+    plt.rcParams.update({'figure.figsize': (7, 5)})
 
     norm = Normalize(vmin=5, vmax=26)
     orig_color = "steelblue"
@@ -162,7 +162,7 @@ def plot_grouped_bars(results, x_list, x_label, y_label, output_prefix,
             color = cmaps[backend](norm(pre_issue_depth))
             hatch = ''
             if config.startswith("io_uring"):
-                label = f"foreactor-io_uring-{pre_issue_depth}"
+                label = f"foreactor-{pre_issue_depth}"
             else:
                 label = f"foreactor-thread_pool-{pre_issue_depth}"
         
@@ -174,7 +174,7 @@ def plot_grouped_bars(results, x_list, x_label, y_label, output_prefix,
             overall_max_ys = max_ys
 
     for idx, config in enumerate(results.keys()):
-        xs = list(map(lambda x: x * (len(results)+1.2) + idx,
+        xs = list(map(lambda x: x * (len(results)+1.2) + idx + 0.2,
                       range(len(x_list))))
         ys = results[config]
         for x, y in zip(xs, ys):
@@ -187,17 +187,25 @@ def plot_grouped_bars(results, x_list, x_label, y_label, output_prefix,
 
     xticks = list(map(lambda x: x * (len(results)+1.2) + (len(results)/4),
                       range(len(x_list))))
-    plt.xticks(xticks, x_list)
+    x_list_pruned = [s if not s.endswith('B') else s[:-1] for s in x_list]
+    plt.xticks(xticks, x_list_pruned, fontsize=17)
 
-    plt.grid(axis='y', zorder=1)
+    plt.yticks(fontsize=17)
 
-    plt.ylim((0, overall_max_ys * 1.15))
+    # plt.grid(axis='y', zorder=1)
+    
+    if overall_max_ys > 1000:
+        plt.ylim((0, overall_max_ys * 1.3))
+    else:
+        plt.ylim((0, overall_max_ys * 1.25))
 
-    plt.legend(loc="center left", bbox_to_anchor=(1.02, 0.5))
+    plt.legend(mode="expand", ncol=3, loc="lower left",
+               bbox_to_anchor=(0, 1, 1, 0.2),
+               fontsize=20, frameon=False)
 
     plt.tight_layout()
 
-    plt.savefig(f"{output_prefix}-{title_suffix}.png", dpi=200)
+    plt.savefig(f"{output_prefix}-{title_suffix}.pdf", dpi=200)
     plt.close()
     print(f"PLOT {title_suffix}")
 
@@ -232,8 +240,8 @@ def handle_mem_ratio(results_dir, output_prefix):
                     results[config].append(avg_us)
 
         plot_grouped_bars(results, x_list,
-                          "Available memory vs. DB volume (%)",
-                          "Avg. time per Get request (us)",
+                          "Available mem vs. DB volume (%)",
+                          "Time per Get (us)",
                           output_prefix, f"mem_ratio-req_{value_size}")
 
 def handle_req_size(results_dir, output_prefix):
@@ -268,7 +276,7 @@ def handle_req_size(results_dir, output_prefix):
 
         plot_grouped_bars(results, x_list,
                           "Record value size (bytes)",
-                          "Avg. time per Get request (us)",
+                          "Time per Get (us)",
                           output_prefix, f"req_size-mem_{mem_percentage}")
 
 def handle_tail_lat(results_dir, output_prefix):
@@ -303,14 +311,14 @@ def handle_tail_lat(results_dir, output_prefix):
 
         plot_grouped_bars(results, x_list,
                           "Record value size (bytes)",
-                          "P99 tail latency of Get request (us)",
+                          "P99 tail latency (us)",
                           output_prefix, f"tail_lat-mem_{mem_percentage}")
 
 
 def plot_heat_map(dist_results, distributions, x_list, y_list, x_label, y_label,
                   output_prefix, title_suffix):
-    plt.rcParams.update({'font.size': 16})
-    plt.rcParams.update({'figure.figsize': (6, 6)})
+    plt.rcParams.update({'font.size': 20})
+    plt.rcParams.update({'figure.figsize': (5, 5)})
 
     vmin, vmax = -0.35, 0.45
     cmin, cmax = -0.1, 0.4
@@ -332,8 +340,9 @@ def plot_heat_map(dist_results, distributions, x_list, y_list, x_label, y_label,
         if sp_idx == 0:
             plt.ylabel(y_label)
 
-        plt.xticks(list(range(len(x_list))), x_list)
-        plt.yticks(list(range(len(y_list))), y_list)
+        y_list_pruned = [s if not s.endswith('B') else s[:-1] for s in y_list]
+        plt.xticks(list(range(len(x_list))), x_list, fontsize=16)
+        plt.yticks(list(range(len(y_list))), y_list_pruned, fontsize=16)
 
         if len(distributions) > 1:
             plt.title(distribution.capitalize(), fontsize=16)
@@ -343,15 +352,15 @@ def plot_heat_map(dist_results, distributions, x_list, y_list, x_label, y_label,
     plt.subplots_adjust(right=0.9)
 
     fig = plt.figure(1)
-    fig.supxlabel(x_label, fontsize=16, va="center")
+    fig.supxlabel(x_label, fontsize=20, va="center")
 
     cbar_ax = fig.add_axes([0.92, 0.15, 0.02, 0.7])
     cbar_ticks = list(np.arange(cmin, cmax+0.1, 0.1))
     cbar_labels = list(map(lambda p: f"{p*100:.0f}%", cbar_ticks))
     cbar = plt.colorbar(cax=cbar_ax, ticks=cbar_ticks, shrink=0.6)
-    cbar.ax.set_yticklabels(cbar_labels)
+    cbar.ax.set_yticklabels(cbar_labels, fontsize=16)
 
-    plt.savefig(f"{output_prefix}-{title_suffix}.png",
+    plt.savefig(f"{output_prefix}-{title_suffix}.pdf",
                 dpi=200, bbox_inches='tight')
     plt.close()
     print(f"PLOT heat_map")
@@ -385,17 +394,17 @@ def handle_heat_map(results_dir, output_prefix):
 
     plot_heat_map(dist_results, YCSB_DISTRIBUTIONS,
                   MEM_PERCENTAGES, VALUE_SIZES,
-                  "Available memory vs. DB volume (%)",
+                  "Available mem vs. DB volume (%)",
                   "Record value size (bytes)",
                   output_prefix, f"heat_map")
 
 
 def plot_multithread(results, nums_threads, x_label, y_label, output_prefix,
                      title_suffix):
-    plt.rcParams.update({'font.size': 16})
-    plt.rcParams.update({'figure.figsize': (12, 7)})
+    plt.rcParams.update({'font.size': 22})
+    plt.rcParams.update({'figure.figsize': (5, 5)})
 
-    norm = Normalize(vmin=-5, vmax=30)
+    norm = Normalize(vmin=-8, vmax=30)
     orig_color = "steelblue"
     foreactor_cmap = cm.OrRd
     orig_marker = 'o'
@@ -415,10 +424,10 @@ def plot_multithread(results, nums_threads, x_label, y_label, output_prefix,
             pre_issue_depth = int(config)
             color = foreactor_cmap(norm(pre_issue_depth))
             marker = foreactor_marker
-            label = f"foreactor-io_uring-{pre_issue_depth}"
+            label = f"foreactor-{pre_issue_depth}"
         
         plt.plot(xs, ys, label=label, color=color, marker=marker,
-                         linewidth=2.5, markersize=10, zorder=3)
+                         linewidth=2.5, markersize=9, zorder=3)
 
         max_ys = max(ys)
         if max_ys > overall_max_ys:
@@ -435,18 +444,21 @@ def plot_multithread(results, nums_threads, x_label, y_label, output_prefix,
     plt.xlabel(x_label)
     plt.ylabel(y_label)
 
-    plt.xticks(xs, nums_threads)
+    plt.xticks(xs[1::2], nums_threads[1::2], fontsize=17)
+    plt.yticks(fontsize=17)
 
     plt.xlim((min(xs) - 0.05*xs_span, max(xs) + 0.05*xs_span))
     plt.ylim((0, overall_max_ys * 1.1))
 
     plt.grid(axis='y', zorder=1)
 
-    plt.legend(loc="center left", bbox_to_anchor=(1.02, 0.5))
+    plt.legend(mode="expand", ncol=3, loc="lower left",
+               bbox_to_anchor=(-0.18, 1, 1.25, 0.2),
+               fontsize=20, frameon=False, handlelength=1)
 
     plt.tight_layout()
 
-    plt.savefig(f"{output_prefix}-{title_suffix}.png", dpi=200)
+    plt.savefig(f"{output_prefix}-{title_suffix}.pdf", dpi=200)
     plt.close()
     print(f"PLOT {title_suffix}")
 
@@ -454,7 +466,7 @@ def handle_multithread(results_dir, output_prefix):
     VALUE_SIZE_ABBR = "1K"
     YCSB_DISTRIBUTION = "zipf_0.99"
     BACKEND = "io_uring_sqe_async"
-    PRE_ISSUE_DEPTH_LIST = [4, 16]
+    PRE_ISSUE_DEPTH_LIST = [16]
     MEM_PERCENTAGE = 10
     NUMS_THREADS = [i+1 for i in range(16)]
 
@@ -477,15 +489,15 @@ def handle_multithread(results_dir, output_prefix):
             results[str(pre_issue_depth)].append(sum_ops / y_scale)
 
     plot_multithread(results, NUMS_THREADS,
-                     "Number of threads",
-                     "Throughput (thousand ops/sec)",
+                     "Number of clients",
+                     "Throughput (kops/sec)",
                      output_prefix, f"multithread")
 
 
 def plot_with_writes(results, workload_names, x_label, y_label, output_prefix,
                      title_suffix):
-    plt.rcParams.update({'font.size': 16})
-    plt.rcParams.update({'figure.figsize': (12, 6)})
+    plt.rcParams.update({'font.size': 22})
+    plt.rcParams.update({'figure.figsize': (5, 5)})
 
     norm = Normalize(vmin=5, vmax=26)
     orig_color = "steelblue"
@@ -507,7 +519,7 @@ def plot_with_writes(results, workload_names, x_label, y_label, output_prefix,
             pre_issue_depth = int(config)
             color = foreactor_cmap(norm(pre_issue_depth))
             hatch = ''
-            label = f"foreactor-io_uring-{pre_issue_depth}"
+            label = f"foreactor-{pre_issue_depth}"
         
         plt.bar(xs, ys, width=1, label=label, color=color, hatch=hatch,
                         edgecolor=edge_color, zorder=3)
@@ -523,7 +535,7 @@ def plot_with_writes(results, workload_names, x_label, y_label, output_prefix,
         for x, y in zip(xs, ys):
             shifted_y = y + 0.01 * overall_max_ys
             plt.text(x, shifted_y, f"{y:.1f}", ha="center", va="bottom",
-                                               fontsize=12, rotation=90)
+                                               fontsize=13, rotation=90)
 
     if len(x_label) > 0:
         plt.xlabel(x_label)
@@ -531,18 +543,20 @@ def plot_with_writes(results, workload_names, x_label, y_label, output_prefix,
 
     xticks = list(map(lambda x: x * (len(results)+1.2) + (len(results)/4),
                       range(len(workload_names))))
-    xtick_labels = list(map(lambda n: f"YCSB-{n.upper()}", workload_names))
+    xtick_labels = list(map(lambda n: f"{n.upper()}", workload_names))
     plt.xticks(xticks, xtick_labels)
 
-    plt.grid(axis='y', zorder=1)
+    # plt.grid(axis='y', zorder=1)
 
-    plt.ylim((0, overall_max_ys * 1.1))
+    plt.ylim((0, overall_max_ys * 1.15))
 
-    plt.legend(loc="center left", bbox_to_anchor=(1.02, 0.5))
+    plt.legend(mode="expand", ncol=3, loc="lower left",
+               bbox_to_anchor=(-0.14, 1, 1.2, 0.2),
+               fontsize=20, frameon=False, handlelength=1)
 
     plt.tight_layout()
 
-    plt.savefig(f"{output_prefix}-{title_suffix}.png", dpi=200)
+    plt.savefig(f"{output_prefix}-{title_suffix}.pdf", dpi=200)
     plt.close()
     print(f"PLOT {title_suffix}")
 
@@ -575,15 +589,15 @@ def handle_with_writes(results_dir, output_prefix):
             results[str(pre_issue_depth)].append(sum_ops / y_scale)
 
     plot_with_writes(results, YCSB_WORKLOADS,
-                     "",
-                     "Throughput (thousand ops/sec)",
+                     "YCSB workload",
+                     "Throughput (kops/sec)",
                      output_prefix, f"with_writes")
 
 
 def plot_zipf_consts(rel_uniform, rel_zipfian_list, zipf_constants,
                      x_label, y_label, output_prefix, title_suffix):
-    plt.rcParams.update({'font.size': 16})
-    plt.rcParams.update({'figure.figsize': (10, 6)})
+    plt.rcParams.update({'font.size': 22})
+    plt.rcParams.update({'figure.figsize': (5, 5)})
 
     plt.axhline(y=rel_uniform, label="uniform", color="steelblue",
                                linestyle="--", linewidth=2.5,
@@ -602,19 +616,21 @@ def plot_zipf_consts(rel_uniform, rel_zipfian_list, zipf_constants,
     min_x, max_x = min(zipf_constants), max(zipf_constants)
     min_y, max_y = 1.0, max((rel_uniform, max(rel_zipfian_list)))
     plt.xlim((min_x - 0.05, max_x + 0.05))
-    plt.ylim((min_y, max_y * 1.2))
+    plt.ylim((min_y, max_y * 1.1))
 
     xticks = [min_x + 0.1*i for i in range(len(zipf_constants))]
     xtick_labels = [f"{x:.1f}" for x in xticks]
     plt.xticks(xticks, xtick_labels)
 
-    plt.grid(axis='both', zorder=1)
+    plt.grid(axis='y', zorder=1)
 
-    plt.legend(loc="center left", bbox_to_anchor=(1.02, 0.5))
+    plt.legend(mode="expand", ncol=3, loc="lower left",
+               bbox_to_anchor=(-0.14, 1, 1.2, 0.2),
+               fontsize=20, frameon=False, handlelength=1.5)
 
     plt.tight_layout()
 
-    plt.savefig(f"{output_prefix}-{title_suffix}.png", dpi=200)
+    plt.savefig(f"{output_prefix}-{title_suffix}.pdf", dpi=200)
     plt.close()
     print(f"PLOT {title_suffix}")
 
@@ -653,17 +669,17 @@ def handle_zipf_consts(results_dir, output_prefix):
     
     plot_zipf_consts(rel_uniform, rel_zipfian_list, ZIPF_CONSTANTS,
                      "Zipfian theta constant",
-                     "Relative throughput improvement",
+                     "Relative improvement",
                      output_prefix, f"zipf_consts")
 
 
 def plot_breakdown(fractions_map, segments_us_map, segments, x_label, y_label,
                    output_prefix, title_suffix):
-    plt.rcParams.update({'font.size': 14})
-    plt.rcParams.update({'figure.figsize': (8, 5)})
+    plt.rcParams.update({'font.size': 22})
+    plt.rcParams.update({'figure.figsize': (10, 4)})
 
     segments_cmap = {
-        "peeking-algorithm":        "coral",
+        "pre-issuing-algorithm":    "coral",
         "engine-submission":        "steelblue",
         "engine-completion":        "lightgreen",
         "reflecting-result":        "darkgreen",
@@ -672,7 +688,7 @@ def plot_breakdown(fractions_map, segments_us_map, segments, x_label, y_label,
         "other":                    "lightgray"
     }
     segments_hatch = {
-        "peeking-algorithm":        "xx",
+        "pre-issuing-algorithm":    "xx",
         "engine-submission":        "||",
         "engine-completion":        "\\\\",
         "reflecting-result":        "++",
@@ -694,36 +710,37 @@ def plot_breakdown(fractions_map, segments_us_map, segments, x_label, y_label,
         color = segments_cmap[seg_name]
         hatch = segments_hatch[seg_name]
         
-        h = plt.bar(xs, ys, width=0.4, bottom=bottom_ys, label=seg_name,
-                            color=color, hatch=hatch, edgecolor=edge_color,
-                            zorder=3)
+        h = plt.barh(xs[::-1], ys, height=0.4, left=bottom_ys, label=seg_name,
+                                   color=color, hatch=hatch,
+                                   edgecolor=edge_color, zorder=3)
         
         legend_handles.append(h)
         legend_labels.append(seg_name)
         for cidx, y in enumerate(ys):
             bottom_ys[cidx] += y
 
-    if len(x_label) > 0:
-        plt.xlabel(x_label)
-    plt.ylabel(y_label)
+    if len(y_label) > 0:
+        plt.ylabel(y_label)
+    plt.xlabel(x_label)
 
-    xtick_labels = list(fractions_map.keys())
-    for cidx in range(len(xtick_labels)):
-        if xtick_labels[cidx] != "original":
-            xtick_labels[cidx] = "foreactor"
-    plt.xticks(xs, xtick_labels)
+    ytick_labels = list(fractions_map.keys())
+    for cidx in range(len(ytick_labels)):
+        if ytick_labels[cidx] == "original":
+            ytick_labels[cidx] = "orig."
+        else:
+            ytick_labels[cidx] = "fore."
+    plt.yticks(xs, ytick_labels[::-1])
 
-    plt.grid(axis='y', zorder=1)
-
-    plt.xlim((-0.6, len(fractions_map) - 0.4))
-    plt.ylim((0., max(bottom_ys) * 1.1))
+    plt.xlim((0., max(bottom_ys) * 1.1))
+    plt.ylim((-0.5, len(fractions_map) - 0.5))
 
     plt.legend(reversed(legend_handles), reversed(legend_labels),
-               loc="center left", bbox_to_anchor=(1.02, 0.5))
+               loc="upper left", bbox_to_anchor=(1.01, 1.1),
+               frameon=False, handlelength=1)
 
     plt.tight_layout()
 
-    plt.savefig(f"{output_prefix}-{title_suffix}.png", dpi=200)
+    plt.savefig(f"{output_prefix}-{title_suffix}.pdf", dpi=200)
     plt.close()
     print(f"PLOT {title_suffix}")
 
@@ -735,11 +752,11 @@ def handle_breakdown(results_dir, output_prefix):
     MEM_PERCENTAGE = 10
 
     SEGMENTS = [
-        "peeking-algorithm",
-        "engine-submission",
-        "engine-completion",
-        "reflecting-result",
         "clearing-cancellation",
+        "reflecting-result",
+        "engine-completion",
+        "engine-submission",
+        "pre-issuing-algorithm",
         "synchronous-syscall",
         "other"
     ]
@@ -749,7 +766,7 @@ def handle_breakdown(results_dir, output_prefix):
         for seg_name, seg_val in d.items():
             if seg_name == "get-frontier" or seg_name == "check-args" or \
                seg_name == "peek-algo" or seg_name == "push-forward":
-                result["peeking-algorithm"] += seg_val
+                result["pre-issuing-algorithm"] += seg_val
             elif seg_name == "sync-call":
                 result["synchronous-syscall"] += seg_val
             elif seg_name == "clear-prog" or seg_name == "reset-graph":
@@ -783,9 +800,10 @@ def handle_breakdown(results_dir, output_prefix):
         fractions_map[str(pre_issue_depth)] = timer_segs_to_plot_segs(fractions)
         segments_us_map[str(pre_issue_depth)] = timer_segs_to_plot_segs(segments_us)
 
+    # print(segments_us_map)
     plot_breakdown(fractions_map, segments_us_map, SEGMENTS,
-                   "",
                    "Latency breakdown (us)",
+                   "",
                    output_prefix, f"breakdown")
 
 
